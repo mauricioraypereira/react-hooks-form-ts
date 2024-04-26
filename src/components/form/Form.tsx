63777f2770dc724e118fe1a8a007631ef757782c
Mauricio Ray <mauricioraypereira@gmail.com>
2024-04-26 09:54:02 -0300

    Using default styles on inputs with tailwind

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -9,18 +9,19 @@ const Form = () => {
   }
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div>
+    <form onSubmit={handleSubmit(onSubmit)} className="w-96 bg-white p-4 rounded-md border border-slate-300">
+      <div className="flex flex-col gap-2 mb-2">
         <label htmlFor="name">Nome:</label>
         <input 
           type="text" 
           id="name" 
           placeholder="Digite seu nome" 
-          {...register("name")} 
+          {...register("name")}
+          className="h-10 pl-2 rounded-md border border-slate-300" 
         />
-        {errors.name && (<small>{errors.name.message}</small>)}
+        {errors.name && (<small className="text-red-500 italic">{errors.name.message}</small>)}
       </div>
-      <button type="submit">Finalizar cadastro</button>
+      <button type="submit" className="w-full h-11 bg-cyan-400 rounded-md my-2 hover:bg-cyan-500 transition-all">Finalizar cadastro</button>
     </form>
   )
 };
