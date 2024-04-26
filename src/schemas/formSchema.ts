3cdb24645ab45243dc03d44b83165636449e4003
Mauricio Ray <mauricioraypereira@gmail.com>
2024-04-26 09:38:03 -0300

    Creating schema using zod lib

---
+++ src/schemas/formSchema.ts
@@ -0,0 +1,35 @@
+import { z } from 'zod';
+
+export const formSchema = z.object({
+    name: z
+        .string()
+        .min(3,  'O Número mínimo de caracteres é 3.'),
+    lastname: z
+        .string()
+        .min(3,  'O Número mínimo de caracteres é 3.'),
+    gender: z
+        .string()
+        .refine((field) => field !== "select", {
+            message: "Você precisa escolher um gênero"
+        }),
+    email: z
+        .string()
+        .min(1, "O campo é obrigatório")
+        .email("Utilize um e-mail válido."),
+    password: z
+        .string()
+        .min(6, "A senha precisa ter no mínimo 6 caracteres."),
+    confirmpassword: z
+        .string()
+        .min(6, "A confirmação de senha precisa ter no mínimo 6 caracteres."),
+    agreeterms: z
+        .boolean()
+        .refine((field) => field === true, {
+            message: "Você precisa concordar com os termos."
+        }),
+}).refine((field) => field.password === field.confirmpassword, {
+    message: "As senhas precisam ser iguais.",
+    path: ["confirmpassword"],
+});
+
+export type FormSchema = z.infer<typeof formSchema>;
