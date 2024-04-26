4f92fed03da61fbfc1364c49a722314be18e2c8b
Mauricio Ray <mauricioraypereira@gmail.com>
2024-04-26 09:28:42 -0300

    Creating project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Form from "./components/form/Form";
+
+function App() {
+  return (
+    <div>
+      <h1 className="font-bold text-3xl">React Hook Form com TypeScript</h1>
+      <Form />
+    </div>
+  )
+}
+
+export default App;
