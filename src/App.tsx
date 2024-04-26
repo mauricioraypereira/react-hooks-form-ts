f0e7ec03560b674569d24858901e0fdb2c90b945
Mauricio Ray <mauricioraypereira@gmail.com>
2024-04-26 09:49:23 -0300

    Using validations on form

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Form from "./components/form/Form";
 
 function App() {
   return (
-    <div>
+    <div className="bg-zinc-100 min-h-screen w-full, flex flex-col items-center gap-6 pt-8">
       <h1 className="font-bold text-3xl">React Hook Form com TypeScript</h1>
       <Form />
     </div>
--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,6 +1,27 @@
+ import { useForms } from "../../hooks/useForms";
+ import { FormSchema } from "../../schemas/formSchema";
+
 const Form = () => {
+  const { register, handleSubmit, errors } = useForms();
+
+  const onSubmit = (data: FormSchema) => {
+    alert("Dados enviados com sucesso!");
+  }
+
   return (
-    <div>Form</div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="name">Nome:</label>
+        <input 
+          type="text" 
+          id="name" 
+          placeholder="Digite seu nome" 
+          {...register("name")} 
+        />
+        {errors.name && (<small>{errors.name.message}</small>)}
+      </div>
+      <button type="submit">Finalizar cadastro</button>
+    </form>
   )
 };
 
